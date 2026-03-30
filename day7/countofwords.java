4fb05112ff91763ff07c32a6ee69ccbb93e5f468
HARIKUMAR. S <harikumar111106@gmail.com>
2026-03-30 16:36:07 +0000

    Add day 5, day 6, and day 7 solutions

---
+++ day7/countofwords.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class countofwords{   
+public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    String str=sc.nextLine();
+    boolean flag=true;
+    int count=0;
+    for(int i=0;i<str.length();i++){
+        char ch=str.charAt(i);
+        if(ch!=' '){
+            if(flag){
+                flag=false;
+                count++;
+            }
+        }else
+            flag=true;
+    }
+    System.out.println(count);
+}}
